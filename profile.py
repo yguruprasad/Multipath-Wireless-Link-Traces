4d0140c1cef7eb36ae8a09ff8155a7631e33c4d8
Fraida Fund <ffund@nyu.edu>
2023-01-09 12:24:39 -0500

    Update profile script now that it is a Git-backed repo

--- profile.py
+++ profile.py
@@ -17,38 +17,38 @@ request = pc.makeRequestRSpec()
 # Node client
 node_client = request.RawPC('client')
 node_client.disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+cloudlab-PG0:mptcp-apt-kernel'
-node_client.addService(pg.Execute('/bin/sh','/usr/bin/sudo /usr/bin/git clone https://github.com/aydini/Multipath-Wireless-Link-Traces.git /local/repository; /usr/bin/sudo /bin/bash /local/repository/CloudLab/client.sh'))
+node_client.addService(pg.Execute('/bin/sh','/usr/bin/sudo /bin/bash /local/repository/CloudLab/client.sh'))
 iface0 = node_client.addInterface('interface-0', pg.IPv4Address('192.168.10.2','255.255.255.0'))
 iface1 = node_client.addInterface('interface-4', pg.IPv4Address('192.168.20.2','255.255.255.0'))
 
 # Node emulator2
 node_emulator2 = request.RawPC('emulator2')
-node_emulator2.addService(pg.Execute('/bin/sh','/usr/bin/sudo /usr/bin/git clone https://github.com/aydini/Multipath-Wireless-Link-Traces.git /local/repository; /usr/bin/sudo /bin/bash /local/repository/CloudLab/emulator2.sh'))
+node_emulator2.addService(pg.Execute('/bin/sh','/bin/bash /local/repository/CloudLab/emulator2.sh'))
 iface2 = node_emulator2.addInterface('interface-5', pg.IPv4Address('192.168.20.1','255.255.255.0'))
 iface3 = node_emulator2.addInterface('interface-6', pg.IPv4Address('192.168.2.1','255.255.255.0'))
 
 # Node emulator1
 node_emulator1 = request.RawPC('emulator1')
-node_emulator1.addService(pg.Execute('/bin/sh','/usr/bin/sudo /usr/bin/git clone https://github.com/aydini/Multipath-Wireless-Link-Traces.git /local/repository; /usr/bin/sudo /bin/bash /local/repository/CloudLab/emulator1.sh'))
+node_emulator1.addService(pg.Execute('/bin/sh','/usr/bin/sudo /bin/bash /local/repository/CloudLab/emulator1.sh'))
 iface4 = node_emulator1.addInterface('interface-1', pg.IPv4Address('192.168.10.1','255.255.255.0'))
 iface5 = node_emulator1.addInterface('interface-2', pg.IPv4Address('192.168.1.1','255.255.255.0'))
 
 # Node server
 node_server = request.RawPC('server')
 node_server.disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+cloudlab-PG0:mptcp-apt-kernel'
-node_server.addService(pg.Execute('/bin/sh','/usr/bin/sudo /usr/bin/git clone https://github.com/aydini/Multipath-Wireless-Link-Traces.git /local/repository; /usr/bin/sudo /bin/bash /local/repository/CloudLab/server.sh'))
+node_server.addService(pg.Execute('/bin/sh','/usr/bin/sudo /bin/bash /local/repository/CloudLab/server.sh'))
 iface6 = node_server.addInterface('interface-10', pg.IPv4Address('192.168.3.1','255.255.255.0'))
 iface7 = node_server.addInterface('interface-12', pg.IPv4Address('192.168.4.1','255.255.255.0'))
 
 # Node router1
 node_router1 = request.RawPC('router1')
-node_router1.addService(pg.Execute('/bin/sh','/usr/bin/sudo /usr/bin/git clone https://github.com/aydini/Multipath-Wireless-Link-Traces.git /local/repository; /usr/bin/sudo /bin/bash /local/repository/CloudLab/router1.sh'))
+node_router1.addService(pg.Execute('/bin/sh','/usr/bin/sudo /bin/bash /local/repository/CloudLab/router1.sh'))
 iface8 = node_router1.addInterface('interface-7', pg.IPv4Address('192.168.1.2','255.255.255.0'))
 iface9 = node_router1.addInterface('interface-9', pg.IPv4Address('192.168.3.2','255.255.255.0'))
 
 # Node router2
 node_router2 = request.RawPC('router2')
-node_router2.addService(pg.Execute('/bin/sh','/usr/bin/sudo /usr/bin/git clone https://github.com/aydini/Multipath-Wireless-Link-Traces.git /local/repository; /usr/bin/sudo /bin/bash /local/repository/CloudLab/router2.sh'))
+node_router2.addService(pg.Execute('/bin/sh','/usr/bin/sudo /bin/bash /local/repository/CloudLab/router2.sh'))
 iface10 = node_router2.addInterface('interface-3', pg.IPv4Address('192.168.2.2','255.255.255.0'))
 iface11 = node_router2.addInterface('interface-11', pg.IPv4Address('192.168.4.2','255.255.255.0'))
 
